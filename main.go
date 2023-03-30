74a75a08b9fad1d2ce024c349f0e17c5c926b15c
shrtkv <tokoshiro5@gmail.com>
2023-03-29 22:20:18 -0300

    default gin gonic's doc api test

--- main.go
+++ main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// gin router start with the default configs
+	r := gin.Default()
+	//defines a route
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
