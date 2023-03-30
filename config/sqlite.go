87fa546e39fe0db5c4d154645c24f131b3e8df08
shrtkv <tokoshiro5@gmail.com>
2023-03-30 02:27:46 -0300

    feat: add SQLite database creation

---
+++ config/sqlite.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"os"
+
+	"github.com/shrtkv/gopportunities/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+	// Check if DB file exists
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("db file not found, creating...")
+		// Create the DB file and directory
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+	// Create DB and connect
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+	// migrate the schema
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("sqlite automigration error: %v", err)
+		return nil, err
+	}
+	// return the DB
+	return db, nil
+}
