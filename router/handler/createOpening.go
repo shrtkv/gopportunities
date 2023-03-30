2ee9be4cf8d39a9fcf76dd5418b7e395d073f943
shrtkv <tokoshiro5@gmail.com>
2023-03-29 23:53:08 -0300

    feat: add separated files for route handlers

---
+++ router/handler/createOpening.go
@@ -0,0 +1,11 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"msg": "Create Opening"})
+}
---
+++ router/handler/listOpenings.go
@@ -0,0 +1,11 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func ListOpeningsHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"msg": "List Openings"})
+}
