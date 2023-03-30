d98fb236335b59ec0bd759064b13ba74835d7cbd
shrtkv <tokoshiro5@gmail.com>
2023-03-30 00:40:31 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/shrtkv/gopportunities/router"
+import (
+	"github.com/shrtkv/gopportunities/config"
+	"github.com/shrtkv/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
