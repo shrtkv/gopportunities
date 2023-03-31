44bee47f838532ddd5a85710891be3cc91516795
shrtkv <tokoshiro5@gmail.com>
2023-03-31 03:18:53 -0300

    feat:  add request and response helpers

---
+++ router/handler/request.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"fmt"
+)
+
+// create opening
+
+ func errParamIsRequired(name, typ string) error{
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
+ }
+type CreateOpeningRequest struct {
+	Role     string `json: "role"`
+	Company  string `json: "company"`
+	Location string `json: "location"`
+	Remote   *bool `json: "remote"`
+	Link     string `json: "link"`
+	Salary   int64 `json: "salary"`
+}
+
+func (r *CreateOpeningRequest) Validate() error { 
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Salary <= 0 {
+		return fmt.Errorf("request body is empty or malformed")
+	}
+	if r.Role == "" {
+		return errParamIsRequired("role", "string")
+	}
+	if r.Company == "" {
+		return errParamIsRequired("company", "string")
+	}
+	if r.Location == "" {
+		return errParamIsRequired("location", "string")
+	}
+	// if nothing is sent (false or true), remote == nil >> forces user to define
+	if r.Remote == nil {
+		return errParamIsRequired("remote", "bool")
+	}	
+	if r.Link == "" {
+		return errParamIsRequired("link", "string")
+	}	
+	if r.Salary <= 0 {
+		return errParamIsRequired("salary", "int64")
+	}	
+	return nil
+
+}
